c1c14d2f0b90bb5f5fa3961c14400f81386c04da
aoliverg <aoliverg@uoc.edu>
2022-11-14 09:44:01 +0100

    Add files via upload

---
+++ SDL.Trados/MemoryTranslation/Services/ServicesConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SDL.Trados.MTUOC.Services.Cache;
+using SDL.Trados.MTUOC.Services.Http;
+using SDL.Trados.MTUOC.Services.LanguageSupport;
+using SDL.Trados.MTUOC.Services.Tags;
+using SDL.Trados.MTUOC.Services.WebSocketClient;
+using System;
+
+namespace SDL.Trados.MTUOC.Services
+{
+    public static class ServicesConfig
+    {
+        public static IServiceProvider GetProvider()
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddHttpClient()
+                .AddTransient<IHttpService, HttpService>()
+                .AddSingleton<ILanguageSupportService, LanguageSupportService>()
+                .AddSingleton<ITagsServiceV2, TagsServiceV2>()
+                .AddSingleton<ICacheService, CacheService>()
+                .AddSingleton<WebSocketFactory>();
+
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
